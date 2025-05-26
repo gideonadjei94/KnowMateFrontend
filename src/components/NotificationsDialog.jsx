d989064bacd53d748836fe3f87a397508da17ca8
gideonadjei <gideonadjei94@gmail.com>
2025-05-26 20:24:34 +0000

    Quiz Outlet

--- src/components/NotificationsDialog.jsx
+++ src/components/NotificationsDialog.jsx
@@ -1,6 +1,6 @@
 export default function NotificationsDialog() {
   return (
-    <div className="fixed left-60 top-44 z-50 bg-white shadow-lg border-2 border-gray-200 rounded-lg w-[480px] max-h-[280px] overflow-y-auto py-5">
+    <div className="fixed left-60 top-44 z-50 bg-white shadow-lg border-1 border-gray-200 rounded-lg w-[480px] max-h-[280px] overflow-y-auto py-5">
       <div className="p-4 text-sm">
         <h4 className="font-semibold mb-2">Notifications</h4>
       </div>
